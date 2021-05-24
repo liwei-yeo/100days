3af7f6a8898775bdf2a3153c69e07c0a3f8abf5f
liwei-yeo <liwei.yeo.85@gmail.com>
2021-05-25 00:20:45 +0800

    Adding Day6 Reeborg updated with advanced solution

--- Day6-ReeborgMaze.py
+++ Day6-ReeborgMaze.py
@@ -1,17 +1,39 @@
 # https://reeborg.ca/reeborg.html basic maze solution
 # to complete advanced fringe solution on day 15
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    else:
-        if front_is_clear():
-            move()
-        else:
-            turn_left()
+# # Advanced solution to solve edge cases
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     counter = 0
+#     while right_is_clear() and counter < 3:
+#         turn_right()
+#         move()
+#         counter += 1
+#     else:
+#         if front_is_clear():
+#             move()
+#         else:
+#             turn_left()
+#
+#
+# # Easy solution
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     else:
+#         if front_is_clear():
+#             move()
+#         else:
+#             turn_left()
+#
